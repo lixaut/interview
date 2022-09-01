94289f7090ede8fbec994fc2ca5870e8287edb9c
wangli <wanglixaut@qq.com>
2022-09-01 18:31:39 +0800

    递归

--- js/递归.js
+++ js/递归.js
@@ -1,3 +1,5 @@
+
+// 递归求阶乘
 function fn(n) {
     if(n == 1) {
         return 1
@@ -6,3 +8,12 @@ function fn(n) {
 }
 
 console.log(fn(5))
+
+// 求斐波那契数列（兔子序列）
+// 1 1 2 3 5 8 13 ...
+function fn(n) {
+    if(n == 1 || n == 2) {
+        return 1
+    }
+    return fn(n - 1) + fn(n - 2)
+}
